b8574b816b7e6c515d49efd495b77dbca330f45b
Dunia <dunia07@hotmail.com>
2020-11-25 18:22:01 -0300

    Foi realizada a estruturação dos filtros em HTML

--- src/data.js
+++ src/data.js
@@ -1,9 +1,26 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+//export const example = () => {
+//  return 'example';
+//};
 
-export const anotherExample = () => {
-  return 'OMG';
+//export const anotherExample = () => {
+//  return 'OMG';
+//};
+
+export const filtroNome = (rickandmorty) =>{
+  let arrayVazia = [];
+
+  rickandmorty.array.forEach((positionCharacter => {
+   let selectCharacter = (positionCharacter.name.find(
+
+    (character) => character == name
+    ));
+    if (selectCharacter != undefined){
+      arrayVazia.push(positionCharacter);
+    }
+  }));
+  console.log (arrayVazia)
+    return arrayVazia;
+
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-import { example } from './data.js';
+//import { example } from './data.js';
+// import data from './data/lol/lol.js';
+//import data from './data/pokemon/pokemon.js';
+// import data from './data/rickandmorty/rickandmorty.js';
+
+//console.log(example, data);
+
+import { filtroNome } from './data.js';
 // import data from './data/lol/lol.js';
 //import data from './data/pokemon/pokemon.js';
  import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//console.log(example, data);
+
+//const nameCharacter = document.querySelector
+console.log (data)
